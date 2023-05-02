ad42a4c9929cf981ef88236c27345efa02fe8afd
Kumar <chako@chakos-MacBook-Air.local>
2023-05-02 15:19:31 +0530

    alert added to specify if dark mode or light has been enabled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import Alert from './components/Alert';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
@@ -7,29 +8,40 @@ import React, { useState } from 'react'
 
 
 function App() {
-  
-  const [mode, setMode] = useState("light");
-  const toggleMode=()=>{
-    if(mode === "light"){
-      setMode("dark");
-      document.body.style.backgroundColor ="#042743"
-    
-    }
-    else{
-      setMode("light")
-      document.body.style.backgroundColor ="white"
 
+  const [mode, setMode] = useState("light");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743"
+      showAlert("Dark mode is enabled", "success")
+    }
+    else {
+      setMode("light")
+      document.body.style.backgroundColor = "white"
+      showAlert("light mode is enabled", "success")
     }
   }
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => { // this will remove the alert within 1.5 seconds 
+      setAlert(null)
+    }, 1500);
+  }
   return (
-   <>
-   
-   <Navbar mode={mode} toggleMode={toggleMode}/>
-    <div className = "container my-3">  
-    <TextForm heading="Enter the text to Analyze." mode={mode}/>
-    {/* <About/> */}
-   </div>
-   </>
+    <>
+
+      <Navbar mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+      <TextForm heading="Enter the text to Analyze." mode={mode} />
+        {/* <About/> */}
+      </div>
+    </>
   );
 }
 
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Alert(props) {
     return (
-        <>
+        props.alert && <>
             <div>
-                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                    <strong>{props.alert}</strong>
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                    <strong>{props.alert.type}</strong>: {props.alert.msg}
+                    
                 </div>
 
             </div>
